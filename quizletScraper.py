93ca28509ae663ee28161374f7682b69f3453065
Akhilesh Gulati <agulati235@gmail.com>
2018-04-30 16:35:42 +0530

    major one: scrap words from gsheet and take diff

--- quizletScraper.py
+++ quizletScraper.py
@@ -8,7 +8,28 @@ r  = requests.get(url)
 data = r.text
 
 soup = BeautifulSoup(data)
-
 wtag = soup.select('.SetPageTerm-wordText > .TermText')
+allw = [i.get_text().encode("utf-8") for i in wtag]
 
-w = [i.get_text().encode("utf-8") for i in wtag]
+
+# words from google sheet
+a = """trite    sanction    intrepid    pernicious
+hegemony    irrevocable dilettante  
+ravenous    obsequious  poignant    winsome
+arcane  tractable   inscrutable precipitous
+munificent  resurgent   rescind haphazard
+garrulous   constituent reverent    
+prolific    tempered    myriad  
+taciturn        pernicious  
+sycophant       circumscribe    
+sporadic        inimical    
+subversive      inveterate  
+        repudiate   """
+a = re.sub('\n', ' ', a)
+a = re.sub(' +', ' ', a)
+b = a.split()
+writtenw = b
+
+
+
+remaining = [x for x in allw if x not in writtenw]
